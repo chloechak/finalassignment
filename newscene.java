fdb599eed52a4024bcf80143a873ebeb1094f52c
chloechak <chloechak0@gmail.com>
2021-05-29 14:17:20 -0400

    new scenes

--- newscene.java
+++ newscene.java
@@ -20,8 +20,13 @@ import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import javafx.scene.control.cell.PropertyValueFactory;
 
 import javax.swing.JOptionPane;
+import javax.swing.text.TableView;
 
 public class newscene extends Application {
     Scene scene1, scene2;
@@ -405,5 +410,116 @@ class Characters extends scenes {
         window.show();
 
 
+    }
+}
+class scenes3 extends scenes {
+    private SimpleStringProperty pageNumber, Topic, Description;
+
+    String getPageNumber() {
+        return pageNumber.get();
+    }
+
+    String getTopicrName() {
+        return Topic.get();
+    }
+
+    String getDescription() {
+        return Description.get();
+    }
+    
+    public void Record(String pageNumber, String Topic, String Description) {
+        this.pageNumber = new SimpleStringProperty(pageNumber);
+        this.Topic = new SimpleStringProperty(Topic);
+        this.Description = new SimpleStringProperty(Description);
+    
+    }
+
+    private final static TableView<Record> tableView = new TableView<>();
+    private final static ObservableList<Record> dataList = FXCollections.observableArrayList();
+
+    public static void display (String value) { 
+        Stage window2 = new Stage();
+        Scene scene3 = new Scene(new Group(), 850, 450);
+
+        // Character read notes scene 
+        TableColumn columnA1 = new TableColumn("Page Number");
+        columnA1.setCellValueFactory(new PropertyValueFactory<>("Page Number"));
+ 
+        TableColumn columnA2 = new TableColumn("Topic");
+        columnA2.setCellValueFactory(new PropertyValueFactory<>("Topic"));
+ 
+        TableColumn columnA3 = new TableColumn("Details");
+        columnA3.setCellValueFactory(new PropertyValueFactory<>("Details"));
+
+        // // Literary Devices read notes scene 
+        // TableColumn columnB1 = new TableColumn("Page Number");
+        // columnB1.setCellValueFactory(new PropertyValueFactory<>("Page Number"));
+ 
+        // TableColumn columnB2 = new TableColumn("Literary Device");
+        // columnB2.setCellValueFactory(new PropertyValueFactory<>("Literary Device"));
+ 
+        // TableColumn columnB3 = new TableColumn("Explanation");
+        // columnB3.setCellValueFactory(new PropertyValueFactory<>("Explanation"));
+
+        // // Techniques read notes scene 
+        // TableColumn columC1 = new TableColumn("Page Number");
+        // columnA1.setCellValueFactory(new PropertyValueFactory<>("Page Number"));
+ 
+        // TableColumn columnC2 = new TableColumn("Character Name");
+        // columnA2.setCellValueFactory(new PropertyValueFactory<>("Character Name"));
+ 
+        // TableColumn columnC3 = new TableColumn("Character Notes");
+        // columnA3.setCellValueFactory(new PropertyValueFactory<>("Character Notes"));
+
+        tableView.setItems(dataList);
+        Group root = (Group)scene3.getRoot();
+
+
+        switch (value) {
+            case "Characters" : {
+                tableView.getColumns().addAll(columnA1, columnA2, columnA3);
+                root.getChildren().add(tableView);
+
+                break;
+            }
+            case "Literary Devices" : {
+                tableView.getColumns().addAll(columnA1, columnA2, columnA3);
+                root.getChildren().add(tableView);
+
+                break;
+            } 
+            case "Techniques" : {
+                tableView.getColumns().addAll(columnA1, columnA2, columnA3);
+                root.getChildren().add(tableView);
+
+                break;
+            } 
+            case "Themes" : {
+                tableView.getColumns().addAll(columnA1, columnA2, columnA3);
+                root.getChildren().add(tableView);
+
+                break;
+            } 
+            case "Important Quotes" : {
+                tableView.getColumns().addAll(columnA1, columnA2, columnA3);
+                root.getChildren().add(tableView);
+
+                break;
+            } 
+            case "Plot Analysis" : {
+                tableView.getColumns().addAll(columnA1, columnA2, columnA3);
+                root.getChildren().add(tableView);
+
+                break;
+            } 
+            case "Additional Notes" : {
+                tableView.getColumns().addAll(columnA1, columnA2, columnA3);
+                root.getChildren().add(tableView);
+
+                break;
+            }
+        }
+        window2.setScene(scene3);
+        window2.show();
     }
 }
